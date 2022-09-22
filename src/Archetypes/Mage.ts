a746bf44cd07e1730797575165ad8fc37c53f058
saraivais <isadorasaraiva95@gmail.com>
2022-09-22 14:55:08 -0300

    feat [Mage]: create Mage class~

--- src/Archetypes/Mage.ts
+++ src/Archetypes/Mage.ts
@@ -1,1 +1,23 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
 
+class Mage extends Archetype {
+  private _energyType: EnergyType;
+  static instances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+    Mage.instances += 1;
+  }
+  
+  static createdArchetypeInstances(): number {
+    return Mage.instances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
+
+export default Mage;
