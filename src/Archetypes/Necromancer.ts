6fb4d41bed648b65ea0bd0af3b5ad7d133720a31
saraivais <isadorasaraiva95@gmail.com>
2022-09-22 14:55:23 -0300

    feat [Necromancer]: create Necromancer class~

--- src/Archetypes/Necromancer.ts
+++ src/Archetypes/Necromancer.ts
@@ -1,1 +1,23 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
 
+class Necromancer extends Archetype {
+  private _energyType: EnergyType;
+  static instances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+    Necromancer.instances += 1;
+  }
+  
+  static createdArchetypeInstances(): number {
+    return Necromancer.instances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
+
+export default Necromancer;
