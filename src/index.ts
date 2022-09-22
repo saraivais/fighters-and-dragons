e677fb779196f240880ea7b03c237b156d88f04c
saraivais <isadorasaraiva95@gmail.com>
2022-09-22 18:26:09 -0300

    feat [index]: imports all necessary stuff~

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+import { Ranger, Warrior } from './Archetypes';
+import Character from './Character';
+import { Dwarf, Halfling, Elf } from './Races';
+import Monster from './Monster';
+import Dragon from './Dragon';
+import getRandomInt from './utils';
+import Battle, { PVE, PVP } from './Battle';
 
