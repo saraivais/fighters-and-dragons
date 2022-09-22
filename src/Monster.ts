b6460ac68485563716e22fde51737c0c5f93eefd
saraivais <isadorasaraiva95@gmail.com>
2022-09-22 16:46:30 -0300

    feat [Monster]: creates Monster class~

--- src/Monster.ts
+++ src/Monster.ts
@@ -1,1 +1,34 @@
+import { SimpleFighter } from './Fighter';
 
+class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    if ((this._lifePoints - attackPoints) <= 0) {
+      this._lifePoints = -1;
+    } else {
+      this._lifePoints -= attackPoints;
+    }
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
+
+export default Monster;
