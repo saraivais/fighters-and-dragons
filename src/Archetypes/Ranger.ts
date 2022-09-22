2ac7df002e28bb59bb24636f85e1f1b06a856ff2
saraivais <isadorasaraiva95@gmail.com>
2022-09-22 14:55:39 -0300

    feat [Ranger]: create Ranger class~

--- src/Archetypes/Ranger.ts
+++ src/Archetypes/Ranger.ts
@@ -1,1 +1,23 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
 
+class Ranger extends Archetype {
+  private _energyType: EnergyType;
+  static instances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Ranger.instances += 1;
+  }
+  
+  static createdArchetypeInstances(): number {
+    return Ranger.instances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
+
+export default Ranger;
