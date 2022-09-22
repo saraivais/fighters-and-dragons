444e95653880088cd029f40de5bc4349cf436ba3
saraivais <isadorasaraiva95@gmail.com>
2022-09-22 14:55:52 -0300

    feat [Warrior]: create Warrior class~

--- src/Archetypes/Warrior.ts
+++ src/Archetypes/Warrior.ts
@@ -1,1 +1,23 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
 
+class Warrior extends Archetype {
+  private _energyType: EnergyType;
+  static instances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Warrior.instances += 1;
+  }
+  
+  static createdArchetypeInstances(): number {
+    return Warrior.instances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
+
+export default Warrior;
