963fc997474d51a2f7262ccc386feb1065a41791
saraivais <isadorasaraiva95@gmail.com>
2022-09-22 14:37:46 -0300

    feat [Halfling]: extends Halfling class from Race~

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Halfling extends Race {
+  private _maxLifePoints: number;
+  static instances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints= 60;
+    Halfling.instances += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return Halfling.instances;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
+
+export default Halfling;
