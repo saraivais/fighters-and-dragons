355a4bb12702b73283cbeb24306bb73186b3dbb5
saraivais <isadorasaraiva95@gmail.com>
2022-09-22 18:26:31 -0300

    feat [index]: creates players~

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,14 @@ import Dragon from './Dragon';
 import getRandomInt from './utils';
 import Battle, { PVE, PVP } from './Battle';
 
+// create players~
+const myElf = new Elf('Luric Ithamecath', getRandomInt(1, 10));
+const myRanger = new Ranger(myElf.name);
+const player1 = new Character(myElf.name, myElf, myRanger);
+
+const myDwarf = new Dwarf('Guldur Tollack', getRandomInt(1, 10));
+const myWarrior = new Warrior(myDwarf.name);
+const player2 = new Character(myDwarf.name, myDwarf, myWarrior);
+
+const myHalfling = new Halfling('Hilda Keenbloom', getRandomInt(1, 10));
+const player3 = new Character(myHalfling.name, myHalfling);
