49478675a0e489891520c29313af2a6d13fc0ed5
saraivais <isadorasaraiva95@gmail.com>
2022-09-22 14:38:00 -0300

    feat [Orc]: extends Orc class from Race~

---
+++ src/Races/Orc.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Orc extends Race {
+  private _maxLifePoints: number;
+  static instances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints= 74;
+    Orc.instances += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return Orc.instances;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
+
+export default Orc;
